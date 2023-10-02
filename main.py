9adede4df404916d8d7b31672982f84a49e7c263
BEKIRKSU <bekirkscu@gmail.com>
2023-10-02 17:23:38 +0100

    Array for new turtles to be made and added into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-
+import random
 
 is_race_on = False
 # The race doesn't start
@@ -11,24 +11,30 @@ user_choice = screen.textinput(title="Who will win?", prompt="which turtle do yo
 # This works just like an input so we just assign it to a variable.
 colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
 y_positions = [-70, -30, 10, 50, 90, 130]
+all_turtles = []
 
 for turtle_index in range(0,6):
-    shell = Turtle(shape="turtle")
-    shell.color(colors[turtle_index])
-    shell.shapesize(2, 2)
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.shapesize(2, 2)
 # We can just set the shape when we set the name.
-    shell.penup()
+    new_turtle.penup()
 # We won't do pen down since it's a race and not a drawing.
 # We want the turtle to go to the left of the screen, and we will have more turtles which should all go the same place.
 # We can do this with the 'goto' method below, and using the width and height we can adjust this. The turtle should go
 # to the left, which means an x value of -350 (or a little less) on a graph of 700 width since it starts in the middle.
-    shell.goto(x=-330, y=y_positions[turtle_index])
-# Now we can just make more.
+    new_turtle.goto(x=-330, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
 if user_choice:
     # If user choice input is triggered then race is on.
     is_race_on = True
 
+while is_race_on:
+    #This way the while loop only starts when is_race_on is true and hence after the user has made a choice.
+    random_distance = random.randint(0, 10)
+    turtle.forward(random_distance)
+    
 
 
 screen.exitonclick()

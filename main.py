892bd9049d1852f68de4f7d23217b511a3caa364
BEKIRKSU <bekirkscu@gmail.com>
2023-10-02 17:09:01 +0100

    All turtles lined up

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 
 screen = Screen()
@@ -7,13 +8,21 @@ screen.setup(width=700, height=700)
 # arguments rather than positional arguments.
 user_choice = screen.textinput(title="Who will win?", prompt="which turtle do you think will win? Choose a colour: ")
 # This works just like an input so we just assign it to a variable.
-print(user_choice)
+colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+y_positions = [-70, -30, 10, 50, 90, 130]
 
-shell = Turtle()
+for turtle_index in range(0,6):
+    shell = Turtle(shape="turtle")
+    shell.color(random.choice(colors))
+    shell.shapesize(2, 2)
+# We can just set the shape when we set the name.
+    shell.penup()
+# We won't do pen down since it's a race and not a drawing.
 # We want the turtle to go to the left of the screen, and we will have more turtles which should all go the same place.
 # We can do this with the 'goto' method below, and using the width and height we can adjust this. The turtle should go
 # to the left, which means an x value of -350 (or a little less) on a graph of 700 width since it starts in the middle.
-shell.goto(x=-335, y=0)
+    shell.goto(x=-330, y=y_positions[turtle_index])
+# Now we can just make more.
 
 
 
